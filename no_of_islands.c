3c0c2e69b05e9be4f52e6505eeb2402b753d62c1
Shubham Bastola <144554788+shubhambastola@users.noreply.github.com>
2025-04-29 14:11:15 -0500

    Add files via upload
    No of island file added

---
+++ no_of_islands.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void dfs(char** grid, int gridSize, int* gridColSize, int i, int j) {
+    if (i < 0 || i >= gridSize || j < 0 || j >= gridColSize[i] || grid[i][j] == '0') {
+        return;
+    }
+    grid[i][j] = '0';
+    dfs(grid, gridSize, gridColSize, i + 1, j);
+    dfs(grid, gridSize, gridColSize, i - 1, j);
+    dfs(grid, gridSize, gridColSize, i, j + 1);
+    dfs(grid, gridSize, gridColSize, i, j - 1);
+}
+int numIslands(char** grid, int gridSize, int* gridColSize) {int count=0;
+    for(int i=0;i<gridSize;i++){for(int j=0;j<gridColSize[i];j++){
+        if(grid[i][j]=='1'){count+=1;}  dfs(grid, gridSize, gridColSize, i, j);           }    }
+    return count;
+}
